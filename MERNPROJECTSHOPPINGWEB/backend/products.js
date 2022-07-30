c2c3e496233495603931acd88622cda5721014ca
MeenakshiMohapatra07 <88267898+Codcaptain@users.noreply.github.com>
2022-07-30 10:36:35 +0530

    Teckstack used -->Frontend using reactjs-redux and Backend using nodejs
    [Here , i have done a project on mern stack using virtual doms as such by React as it is efficient debugging and error-checking with unidirectional data flow.]
    1>in the frontend i had created some reqd components in the src as like components and slices where such as the  file NavBar where i designed the heading of the page also activate the direct access to the link on heading part.
    #Also it had sort of packages like router-redux for usedispatch for assign of the information,useselector for getting the no. of cart items.
    #then in Home page to show all the items added by the admin
    #in the cart.jsx to present the items selected by the user
    #auth folder checks the authuntication of the user from registration to login
    #silces folder has all the access of the admin and user , product addition to cart
    #used packages are react,react-router-dom,axios ,router,jwt-decode,@reduxjs/toolkit,styled-components,react-toastify
    
    *****************************************************************************************************************************************
    2>In Backend 
    #in index.js imported all the packages also middleware function router declared ,links to the specific user input page
    #checking authentication of those links in generateAuthToken
    #also used  api's for adding and getting the data in mongodb and jwt also in the serverside
    #used packages express,mongoose,bycrypt,cors,jsonwebtoken,joi....

---
+++ MERNPROJECTSHOPPINGWEB/backend/products.js
@@ -0,0 +1,31 @@
+const products = [
+    {
+      id: 1,
+      name: "iPhone 12 Pro",
+      brand: "Apple",
+      desc: "6.1-inch display",
+      price: 999,
+      image:
+        "https://m.media-amazon.com/images/I/71xkMA+gvYL._AC_UY218_.jpg",
+    },
+    {
+      id: 2,
+      name: "iPhone 12",
+      brand: "Apple",
+      desc: "5.4-inch mini display",
+      price: 699,
+      image:
+        "https://images-eu.ssl-images-amazon.com/images/I/41xssMLI2DL._SY445_SX342_QL70_FMwebp_.jpg",
+    },
+    {
+      id: 3,
+      name: "Galaxy S",
+      brand: "Samsung",
+      desc: "6.5-inch display",
+      price: 399,
+      image:
+        "https://images-eu.ssl-images-amazon.com/images/I/413fd9o3AgL._SX300_SY300_QL70_FMwebp_.jpg",
+    },
+  ];
+  
+  module.exports = products;
---
+++ MERNPROJECTSHOPPINGWEB/frontend/src/slices/productsApi.js
@@ -0,0 +1,17 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { url } from "./api";
+
+// Define a service using a base URL and expected endpoints
+export const productsApi = createApi({
+  reducerPath: "productsApi",
+  baseQuery: fetchBaseQuery({ baseUrl: `${url}` }),
+  endpoints: (builder) => ({
+    getAllProducts: builder.query({
+      query: () => `products`,
+    }),
+  }),
+});
+
+// auto-generated based on the defined endpoints
+export const { useGetAllProductsQuery } = productsApi;
